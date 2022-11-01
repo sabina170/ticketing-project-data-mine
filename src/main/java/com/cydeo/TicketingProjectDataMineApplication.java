ba92efae519a79af5a3d909e05da024f91c3feb7
Sabina Omarova <sabomar170@gmail.com>
2022-11-01 15:26:40 -0400

    part-1

--- src/main/java/com/cydeo/TicketingProjectDataMineApplication.java
+++ src/main/java/com/cydeo/TicketingProjectDataMineApplication.java
@@ -1,7 +1,9 @@
 package com.cydeo;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TicketingProjectDataMineApplication {
@@ -9,5 +11,12 @@ public class TicketingProjectDataMineApplication {
     public static void main(String[] args) {
         SpringApplication.run(TicketingProjectDataMineApplication.class, args);
     }
+    ModelMapper mapper = new ModelMapper();
+    @Bean
+    public ModelMapper mapper(){
+        return new ModelMapper();
+    }
 
 }
+
+//
--- src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
@@ -1,7 +1,9 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.dto.RoleDTO;
+import com.cydeo.repository.RoleRepository;
 import com.cydeo.service.RoleService;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,9 +11,20 @@ import java.util.List;
 @Service
 public class RoleServiceImpl implements RoleService {
 
+    private final RoleRepository roleRepository;
+
+    public RoleServiceImpl(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
     @Override
     public List<RoleDTO> listAllRoles() {
-        return null;
+        //Controller call me and requesting all RoleDTO, so it can show in the drop-down in the UI
+        //I need to make a call to DB and get all the roles from table
+        //Go to repository and find a service which gives me the roles from DB
+        //How to call any service here? - DI
+        //convert Role to RoleDto by using modelmapper (add dependency in pom.xml)
+        return roleRepository.findAll();
     }
 
     @Override
