c4121448a944eb76293b3374b2605784fa78b6e7
Sabina Omarova <sabomar170@gmail.com>
2022-11-03 12:58:36 -0400

    part-2

--- src/main/java/com/cydeo/service/impl/UserServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/UserServiceImpl.java
@@ -30,11 +30,12 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserDTO findByUserName(String username) {
-        return null;
+        return userMapper.convertToDto(userRepository.findByUserName(username));
     }
 
     @Override
     public void save(UserDTO user) {
+        userRepository.save(userMapper.convertToEntity(user));
 
     }
 
