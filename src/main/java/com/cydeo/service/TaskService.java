b6303227058bcfc298a8d804747dd1e9cd3c5c4b
Sabina Omarova <sabomar170@gmail.com>
2022-11-06 11:44:04 -0500

    part-4

---
+++ src/main/java/com/cydeo/service/TaskService.java
@@ -0,0 +1,17 @@
+package com.cydeo.service;
+
+import com.cydeo.dto.ProjectDTO;
+import com.cydeo.dto.TaskDTO;
+import com.cydeo.dto.UserDTO;
+
+import java.util.List;
+
+public interface TaskService {
+
+    TaskDTO getById(Long id);
+    List<TaskDTO> listAllTasks();
+    void save(TaskDTO dto);
+    void update(TaskDTO dto);
+    void delete(Long id);
+
+}
