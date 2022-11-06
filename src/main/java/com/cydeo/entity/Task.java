d41aac228d03e92797e67e487151d319a85ae023
Sabina Omarova <sabomar170@gmail.com>
2022-11-06 11:35:22 -0500

    part-4

---
+++ src/main/java/com/cydeo/entity/Task.java
@@ -0,0 +1,44 @@
+package com.cydeo.entity;
+
+
+import com.cydeo.dto.ProjectDTO;
+import com.cydeo.dto.UserDTO;
+import com.cydeo.enums.Status;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "tasks")
+@Where(clause = "is_deleted=false")  //for delete function
+public class Task extends BaseEntity{
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="project_id") //here it is not mandatory to put name = "project_id"
+    private Project project;
+
+    @ManyToOne(fetch= FetchType.LAZY)
+    @JoinColumn(name="employee_id")
+    private User assignedEmployee;
+
+    private String taskSubject;
+    private String taskDetail;
+
+    @Enumerated(EnumType.STRING)
+    private Status taskStatus;
+
+    @Column(columnDefinition = "DATE")
+    private LocalDate assignedDate;
+
+
+
+
+
+}
